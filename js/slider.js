a18c915173fb9e28f269caa61c83b5ac0b2dbff7
loltatoez <hairybeck2@gmail.com>
2018-03-07 10:30:56 -0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,5 @@
-
+        var slider = document.getElementById('slider');
+        var output = document.getElementById('valueOutput');
+        slider.oninput = function() {
+            output.innerHTML = this.value;
+        }
