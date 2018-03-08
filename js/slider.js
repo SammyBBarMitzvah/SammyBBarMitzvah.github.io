b15c1f0e604849a44942147bbf08d66972dd9948
loltatoez <hairybeck2@gmail.com>
2018-03-08 10:48:35 -0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,3 @@
-        var slider = document.getElementById('slider');
-        var output = document.getElementById('valueOutput');
-        slider.oninput = function() {
-            output.innerHTML = this.value;
-        }
+
         
 amplify.store('peopleCount', 'slider.value');
