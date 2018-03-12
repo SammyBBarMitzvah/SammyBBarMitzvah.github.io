9c145c61abdc5a750e8a7f9fe9ccfb2e8972a9e3
loltatoez <hairybeck2@gmail.com>
2018-03-12 10:36:25 -0400

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,14 @@
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+
 function submit() {
-    let sliderValue = document.getElementById('slider');
-    let people = {'amount': sliderValue.value};
-    console.log(people.amount);
+ let redirect = "https://sammybbarmitzvah.github.io/peopleAttending="
+ let param = getUrlParameter('peopleAttending');
+ window.location.assign('https://sammybbarmitzvah.github.io/?peopleAttending=' + param);
 }
+
