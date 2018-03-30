503669f948cd61b1af0982624283ef42e26b0289
BuildTools <unconfigured@null.spigotmc.org>
2018-03-30 15:57:39 -0400

    Bork222

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,10 +1,31 @@
+//Form
+var firstName = document.getElementById('firstNameFormInput');
+var lastName = document.getElementById('lastNameFormInput');
+var emailInput = document.getElementById('emailInput');
+var submitButton = document.getElementById('nameFormSubmit');
+
+//Allergies
 var allergies = document.getElementById('allergiesCheckBox');
 var allergiesText = document.getElementById('allergiesTextInput');
-var meal = document.getElementById('mealOption');
-var attendingYes = document.getElementById('attendingRadioYes')
+
+//Attending
+
+var attendingYes = document.getElementById('attendingRadioYes');
 var attendingNo = document.getElementById('attendingRadioNo');
+
+//Form Classes
+var formClass = document.getElementsByClassName('formClass');
+var formAction1 = document.getElementsByClassName('formAction1');
+var formAction2 = document.getElementsByClassName('formAction2')
+
+//Meals
+var steak
+
+//Extras
 var adultCount = document.getElementById('adultCountInput');
-var childCount = document.getElementById('childCountInput')
+var childCount = document.getElementById('childCountInput');
+var formNext = document.getElementById('form1Button');
+
 
 
 // Toggle the required modifer on forms
@@ -12,9 +33,23 @@ function toggleRequired(e) {
     $(e).prop('required', !$(e).prop('required'));
 }
 
+//
+function getTotals() {
+    return;
+}
+
+function checkValidation() {
+    var adults = adultCount.value;
+    var children = childCount.value;
+    if (firstName.value === "" || lastName.value === "" || emailInput.value === "" || adults === "0" && children === "0") {
+        return false;
+    }
+    return true;
+}
 
 $(document).ready(function() {
-    $('.formClass').hide();
+    $(formClass).hide();
+    $(formAction2).hide();
 });
 
 
@@ -24,13 +59,23 @@ $(allergies).click(function() {
     toggleRequired(allergiesText);
 });
 
+$(formNext).click(function() {
+    if (!checkValidation()) {
+        $(formAction1).show();
+    } else {
+        $(formAction1).fadeOut(400);
+        $(formAction2).fadeIn(500);
+        $(allergiesText).hide();
+    }
 
+});
 
 
 $(attendingYes).click(function() {
     if ($(this).is(':checked')) {
         attendingYes.value = 'True'
-        $('.formClass').fadeIn(400);
+        $(formClass).fadeIn(400);
+        $(formAction2).hide();
         $('#allergiesTextInput').hide();
         if ($(allergies).is(':checked') && $(allergiesText).is(':hidden')) {
             $(allergiesText).show();
@@ -43,18 +88,10 @@ $(attendingNo).click(function() {
     attendingNo.value = 'False'
     toggleRequired(adultCount);
     toggleRequired(childCount);
-    $('.formClass').fadeOut(400);
+    $(formClass).fadeOut(400);
+    $(submitButton).fadeIn(300);
     $('#allergiesTextInput').hide();
     if ($(allergies).is(':checked') && $(allergiesText).is(':hidden')) {
         $(allergiesText).show();
     }
-});
-
-
-$('#modalOpen').click(function() {
-    $('.modal').addClass('is-active');
-});
-
-$('#submitSlider').click(function() {
-    $('.modal').removeClass('is-active');
 });
